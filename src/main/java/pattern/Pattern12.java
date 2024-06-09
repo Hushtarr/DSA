927cec9c0d6af916408dfe12d0eab8600e630d2b
hushtar <HUXTAR.EZ@gmail.com>
2024-06-09 19:42:53 +0200

    pattern part update

--- src/main/java/pattern/Pattern12.java
+++ src/main/java/pattern/Pattern12.java
@@ -8,9 +8,9 @@ public class Pattern12 {
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= row; col++) {
                 if ((row + col) % 2 == 0) {
-                    System.out.print("0");
-                } else {
                     System.out.print("1");
+                } else {
+                    System.out.print("0");
                 }
             }
             System.out.println();
